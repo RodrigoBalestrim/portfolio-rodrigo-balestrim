59a3c468bef5598b562f94a7e0371224ff7f8c1a
Rodrigo Balestrim <RodrigoBalestrim@users.noreply.github.com>
2026-08-03 08:11:46 -0300

    Keep background decorations inside sections

--- portfolio-3d/components/Bulb.jsx
+++ portfolio-3d/components/Bulb.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const Bulb = () => {
   return (
-    <div className="pointer-events-none absolute -bottom-12 -left-36 z-10 hidden w-[200px] select-none rotate-12 animate-pulse mix-blend-color-dodge duration-75 sm:block xl:w-[260px]">
+    <div className="pointer-events-none absolute bottom-4 left-4 z-10 hidden w-[160px] select-none rotate-12 animate-pulse mix-blend-color-dodge duration-75 sm:block xl:bottom-6 xl:left-6 xl:w-[220px]">
       <Image
         src="/images/decorations/bulb.png"
         alt="bulb"
--- portfolio-3d/components/Circles.jsx
+++ portfolio-3d/components/Circles.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const Circles = () => {
   return (
-    <div className="pointer-events-none absolute -bottom-2 -right-16 z-10 hidden w-[200px] select-none animate-pulse mix-blend-color-dodge duration-75 sm:block xl:w-[300px]">
+    <div className="pointer-events-none absolute bottom-4 right-20 z-10 hidden w-[180px] select-none animate-pulse mix-blend-color-dodge duration-75 sm:block xl:bottom-6 xl:right-28 xl:w-[240px]">
       <Image
         src="/images/decorations/circles.png"
         alt="circles"
